09cece923dbb14838e58da152344b6459a0a05bd
Santosh Ganti <santosh.ganti@me.com>
2014-01-07 23:24:19 +0530

    changes made on mac

--- problem-solving/src/main/java/problem/solving/Factorial.java
+++ problem-solving/src/main/java/problem/solving/Factorial.java
@@ -1,16 +1,16 @@
 package problem.solving;
-/*Checking if git works from windows */ 
+
 public class Factorial {
-	public double calculateFactorial(int n){
-		//n= number
+	public double calculateFactorial(int n) {
+		// n= number
 		// n! = n*(n-1)*(n-2)......*1
-		
-		double f=1;
-		
-		for(int i=1;i<=n;i++){
-			f=f*i;
+
+		double f = 1;
+
+		for (int i = 1; i <= n; i++) {
+			f = f * i;
 		}
 		return f;
 	}
-	
+
 }
