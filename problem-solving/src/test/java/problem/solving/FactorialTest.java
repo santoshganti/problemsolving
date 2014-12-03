2d1d1736f2de68c55b0216b03fe87db91ece06f7
Santosh <santosh.ganti@me.com>
2014-12-04 01:46:56 +0530

    import static org.junit.Assert.*;
    changed the above import in the Test class. 
    Also, all the imports in future test cases must be changed. Since, the
    default import doesnt have all the methods.

--- problem-solving/src/test/java/problem/solving/FactorialTest.java
+++ problem-solving/src/test/java/problem/solving/FactorialTest.java
@@ -1,23 +1,23 @@
 package problem.solving;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static junit.framework.Assert.*;
-
+//import static junit.framework.Assert.*;
 
 public class FactorialTest {
 	@SuppressWarnings("deprecation")
 	@Test
-	//it is telling the method is a test case
-	//n! = (n-m)! * (m+1) * (m+2) *... n
-	//10! = (10-5)! * 6 * 7 ..10 = 5! * 6 * 7.. 10= 120 * 6 * 7
-	//15! = (15-10)! * 11 *12...
-	public void testFactorial(){
-		
+	// it is telling the method is a test case
+	// n! = (n-m)! * (m+1) * (m+2) *... n
+	// 10! = (10-5)! * 6 * 7 ..10 = 5! * 6 * 7.. 10= 120 * 6 * 7
+	// 15! = (15-10)! * 11 *12...
+	public void testFactorial() {
+
 		Factorial number = new Factorial();
 		assertEquals(120.0, number.calculateFactorial(5));
 		assertEquals(3628800.0, number.calculateFactorial(10));
 		assertEquals(1307674368000.0, number.calculateFactorial(15));
-		
+
 	}
 }
